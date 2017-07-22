982553f542b6cfcdb041dce043e232d7f3c68691
Jayalaxmi Arasalike <jayalaxmiarasalike@Jayalaxmis-MacBook-Pro.local>
2017-07-21 22:15:59 -0700

    get request done

---
+++ app/data/friends.js
@@ -0,0 +1,84 @@
+var friends = [
+	{
+		name: "Jason",
+		photo: "http://www.audiencebloom.com/img/jayson-profile-sew.jpg",
+		scores: [
+			5,
+	      	1,
+	      	4,
+	      	4,
+	      	5,
+	      	1,
+	      	2,
+	      	5,
+	      	4,
+	      	1
+		]
+	},
+	{
+		name: "Albert",
+		photo: "https://www.specialimages.co.uk/blog/wp-content/uploads/2013/06/Linkedin-headshot-copy.jpg",
+		scores: [
+			5,
+	      	4,
+	      	2,
+	      	1,
+	      	5,
+	      	3,
+	      	2,
+	      	5,
+	      	4,
+	      	3
+		]
+	},
+	{
+		name: "Anna",
+		photo: "http://awazpost.com/wp-content/uploads/2014/03/Profile-of-Hansika-Motwani-and-HD-wallpapers-free-download1.jpg",
+		scores: [
+			5,
+	      	1,
+	      	4,
+	      	4,
+	      	4,
+	      	1,
+	      	1,
+	      	5,
+	      	3,
+	      	4
+		]
+	},
+	{
+		name: "Jasmine",
+		photo: "https://upload.wikimedia.org/wikipedia/en/7/7b/Sally_Obermeder_Profile_Image.jpg",
+		scores: [
+			5,
+	      	2,
+	      	4,
+	      	4,
+	      	3,
+	      	1,
+	      	2,
+	      	5,
+	      	4,
+	      	4
+		]
+	},
+	{
+		name: "Greg",
+		photo: "http://cdn3.thr.com/sites/default/files/imagecache/nfe_square_383x383/2015/06/jayson_blair.jpg",
+		scores: [
+			5,
+	      	1,
+	      	4,
+	      	5,
+	      	5,
+	      	1,
+	      	4,
+	      	2,
+	      	4,
+	      	4
+		]
+	}
+];
+
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+// app.get('/', function(req, res){
+// 	res.send('Hello World!');
+// })
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+require('./app/routing/apiRoutes.js')(app, path, express);
+require('./app/routing/htmlRotes.js')(app, path, express);
+
+app.listen(port, function() {
+	console.log("App listening on PORT" + port);
+});
+
+
